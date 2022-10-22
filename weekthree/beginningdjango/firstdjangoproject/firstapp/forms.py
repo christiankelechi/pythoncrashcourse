9f50a6deb4530efb2bb8fee561428b593b8fdbfd
unknown <kezechristian@gmail.com>
2022-10-22 21:49:08 +0100

    pushed last django code sample

--- weekthree/beginningdjango/firstdjangoproject/firstapp/forms.py
+++ weekthree/beginningdjango/firstdjangoproject/firstapp/forms.py
@@ -1,8 +1,11 @@
+from turtle import width
 from django import forms
 from .models import Fruit
+
 class FruitPost(forms.ModelForm):
     name=forms.CharField(widget=forms.TextInput(attrs={'placeholder':"Name of the fruit"}))
     color=forms.CharField(widget=forms.TextInput(attrs={'placeholder':"Color of the fruit"}))
+    description=forms.CharField(widget=forms.Textarea(attrs={'placeholder':'Enter description'}))
     class Meta:
         model=Fruit
         fields=['name','color','description']
